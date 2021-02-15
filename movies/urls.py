da836ba4057d63628744d38d898d932be1c84474
Speccy-rom <speccy.rom@gmail.com>
2021-02-15 23:53:40 +0300

    add serializers (список фильмов, все о фильме)

--- movies/urls.py
+++ movies/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 
 urlpatterns = [
+    path('movie/', views.MoviesListView.as_view()),
+    path('movie/<int:pk>/', views.MoviesDetailView.as_view()),
 
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,2 +1,20 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
+from .models import Movie
+from .serializers import MovieListSerializer, MovieDetailSerializer
+
+
+class MoviesListView(APIView):
+    """Вывод списка фильмов"""
+    def get(self, request):
+        movies = Movie.objects.filter(draft=False)
+        serializer = MovieListSerializer(movies, many=True)
+        return Response(serializer.data)
+
+class MoviesDetailView(APIView):
+    """Вывод фильма"""
+    def get(self, request, pk):
+        movies = Movie.objects.get(id=pk, draft=False)
+        serializer = MovieDetailSerializer(movies)
+        return Response(serializer.data)
