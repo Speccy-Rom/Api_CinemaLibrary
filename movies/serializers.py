53eaa59a31888bb1a355e788f74dfe028b768434
Speccy-rom <speccy.rom@gmail.com>
2021-02-23 23:20:06 +0300

    реализовал рейтинг фильмов (обновление) по ip

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,10 +1,11 @@
 from rest_framework import serializers
 
-from .models import Movie, Review
+from .models import Movie, Review, Rating
 
 
 class FilterReviewListSerializer(serializers.ListSerializer):
     """Фильтр комментариев, только parents"""
+
     def to_representation(self, data):
         data = data.filter(parent=None)
         return super().to_representation(data)
@@ -12,6 +13,7 @@ class FilterReviewListSerializer(serializers.ListSerializer):
 
 class RecursiveSerializer(serializers.Serializer):
     """Вывод рекурсивно children"""
+
     def to_representation(self, value):
         serializer = self.parent.parent.__class__(value, context=self.context)
         return serializer.data
@@ -54,3 +56,19 @@ class MovieDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         exclude = ('draft',)
+
+
+class CreateRatingSerializer(serializers.ModelSerializer):
+    """Добавление рейтинга пользователем"""
+
+    class Meta:
+        model = Rating
+        fields = ('star', 'movie')
+
+    def create(self, validated_data):
+        rating = Rating.objects.update_or_create(
+            ip=validated_data.get('ip', None),
+            movie=validated_data.get('movie', None),
+            defaults={'star': validated_data.get("star")}
+        )
+        return rating
--- movies/urls.py
+++ movies/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('movie/', views.MoviesListView.as_view()),
     path('movie/<int:pk>/', views.MoviesDetailView.as_view()),
     path('review/', views.ReviewCreateView.as_view()),
+    path('rating/', views.AddStarRatingView.as_view()),
 
 ]
